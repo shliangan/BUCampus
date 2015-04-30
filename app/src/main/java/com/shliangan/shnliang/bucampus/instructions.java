2a6e4243f884b56a2717eb8f9e9f9d00ade32327
shnliang <shnliang@yahoo.com>
2015-04-30 17:09:53 -0400

    update 10
    added new key and fixed button

--- app/src/main/java/com/shliangan/shnliang/bucampus/instructions.java
+++ app/src/main/java/com/shliangan/shnliang/bucampus/instructions.java
@@ -52,7 +52,7 @@ public class instructions extends ActionBarActivity {
         maps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(instructions.this, MainActivity.class);
+                Intent intent = new Intent(instructions.this, MapsActivity.class);
                 startActivity(intent);
             }
         });
